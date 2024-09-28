430e8d0d1ca19b00bf56c8867cef12790086877a
Shawn Smith <sds.smith24@gmail.com>
2024-09-28 15:58:44 -0400

    develop api namespace with items controller and cors

---
+++ app/controllers/cors_controller.rb
@@ -0,0 +1,20 @@
+class CorsController < ApplicationController
+
+    def cors_preflight_check
+        if request.method == 'OPTIONS'
+          cors_set_access_control_headers
+          render text: '', content_type: 'text/plain'
+        end
+      end
+
+    protected
+
+    def cors_set_access_control_headers
+        response.headers['Access-Control-Allow-Origin'] = "http://localhost:5173"
+        response.headers['Access-Control-Allow-Credentials'] = "true"
+        response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
+        response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email, X-User-Token, X-User-Email'
+        response.headers['Access-Control-Max-Age'] = '86400'
+    end
+
+end
---
+++ test/controllers/cors_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class CorsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
