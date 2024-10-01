2b95b8a79032323fb09684748134b91e776afdcf
Shawn Smith <sds.smith24@gmail.com>
2024-10-01 13:56:52 -0400

    db reset

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,68 +8,68 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-Role.create(name: "SuperUser")
-Role.create(name: "StaffUser")
+# Role.create(name: "SuperUser")
+# Role.create(name: "StaffUser")
 
-User.create(email: 'sds.smith24@gmail.com', username: 'Admin', user_type: 0, password: '\~/PaPsPdPcPvPb3--', role_id: 1)
+# User.create(email: 'sds.smith24@gmail.com', username: 'Admin', user_type: 0, password: '\~/PaPsPdPcPvPb3--', role_id: 1)
 
-Category.create(name: "Appetizers")
-Category.create(name: "Salads")
-Category.create(name: "Sandwiches")
-Category.create(name: "Entrees")
-Category.create(name: "Kids")
+# Category.create(name: "Appetizers")
+# Category.create(name: "Salads")
+# Category.create(name: "Sandwiches")
+# Category.create(name: "Entrees")
+# Category.create(name: "Kids")
 
-Item.create(
-    name: "Classic Buffalo Wings",
-    description: "Deep fried wings spun with a classic blend of Red Hot and butter.",
-    category: "Appetizers",
-    price: 12.99,
-    calories: 1200,
-    photo_url: 'https://source.unsplash.com/ZTtaJjiF_0M',
-    eightysix: false,
-    archived: false,
-)
+# Item.create(
+#     name: "Classic Buffalo Wings",
+#     description: "Deep fried wings spun with a classic blend of Red Hot and butter.",
+#     category: "Appetizers",
+#     price: 12.99,
+#     calories: 1200,
+#     photo_url: 'https://source.unsplash.com/ZTtaJjiF_0M',
+#     eightysix: false,
+#     archived: false,
+# )
 
-Item.create(
-    name: "Classic Caesar",
-    description: "Fresh crisp Romaine hearts with croutons, shaved parm, and our house-made Caesar dressing.",
-    category: "Salads",
-    price: 10.99,
-    calories: 500,
-    photo_url: 'https://source.unsplash.com/ZTtaJjiF_0M',
-    eightysix: false,
-    archived: false,
-)
+# Item.create(
+#     name: "Classic Caesar",
+#     description: "Fresh crisp Romaine hearts with croutons, shaved parm, and our house-made Caesar dressing.",
+#     category: "Salads",
+#     price: 10.99,
+#     calories: 500,
+#     photo_url: 'https://source.unsplash.com/ZTtaJjiF_0M',
+#     eightysix: false,
+#     archived: false,
+# )
 
-Item.create(
-    name: "Burger",
-    description: "You know, a burger",
-    category: "Sandwiches",
-    price: 6.99,
-    calories: 600,
-    photo_url: 'https://source.unsplash.com/ZTtaJjiF_0M',
-    eightysix: false,
-    archived: false,
-)
+# Item.create(
+#     name: "Burger",
+#     description: "You know, a burger",
+#     category: "Sandwiches",
+#     price: 6.99,
+#     calories: 600,
+#     photo_url: 'https://source.unsplash.com/ZTtaJjiF_0M',
+#     eightysix: false,
+#     archived: false,
+# )
 
-Item.create(
-    name: "Pot Roast",
-    description: "Slow and low chuck roast, mashed potatoes and fresh steamed broccoli.",
-    category: "Entrees",
-    price: 15.99,
-    calories: 825,
-    photo_url: 'https://source.unsplash.com/ZTtaJjiF_0M',
-    eightysix: false,
-    archived: false,
-)
+# Item.create(
+#     name: "Pot Roast",
+#     description: "Slow and low chuck roast, mashed potatoes and fresh steamed broccoli.",
+#     category: "Entrees",
+#     price: 15.99,
+#     calories: 825,
+#     photo_url: 'https://source.unsplash.com/ZTtaJjiF_0M',
+#     eightysix: false,
+#     archived: false,
+# )
 
-Item.create(
-    name: "Grilled Cheese",
-    description: "Melty American cheese and white bread, toasted on the flat top.",
-    category: "Kids",
-    price: 4.99,
-    calories: 300,
-    photo_url: 'https://source.unsplash.com/ZTtaJjiF_0M',
-    eightysix: false,
-    archived: false,
-)
+# Item.create(
+#     name: "Grilled Cheese",
+#     description: "Melty American cheese and white bread, toasted on the flat top.",
+#     category: "Kids",
+#     price: 4.99,
+#     calories: 300,
+#     photo_url: 'https://source.unsplash.com/ZTtaJjiF_0M',
+#     eightysix: false,
+#     archived: false,
+# )
